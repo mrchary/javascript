1d526d35e2bcc60834b5afa3ce294499a743ed2d
unknown <Somrat@Somrat.(none)>
2016-06-09 22:04:30 +0600

    learn javascript

---
+++ date.php
@@ -0,0 +1,7 @@
+<?php include("header.php"); ?>
+<h1>My first JavaScript</h1>
+<button type="button" class="btn btn-primary" onclick="document.getElementById('demo').innerHTML = Date()">
+	Click
+</button>
+<h3 id="demo"></h3>
+<?php include("footer.php"); ?>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Show the time</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body class="container">
