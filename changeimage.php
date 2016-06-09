a70b2fcb48cac466486fea1362e411dc68ae671d
unknown <Somrat@Somrat.(none)>
2016-06-09 22:41:43 +0600

    change image

--- changeimage.php
+++ changeimage.php
@@ -1,16 +1,19 @@
 <?php include("header.php"); ?>
-<h2>JavaScript Can Change Images</h2>
-<img id="myImage" src="images/pic_bulboff.gif" width="100px" height="180px" onclick="changeImage()">
+<h1>JavaScript Can Change Images</h1>
+
+<img id="myImage" onclick="changeImage()" src="images/pic_bulboff.gif" width="100" height="180">
+
 <p>Click the light bulb to turn on/off the light.</p>
-<script type="text/javascript">
-	function changeImage() {
-		var image = document.getElementById('myImage');
-		if(image.src.match("bullbon")) {
-			image.src = "images/pic_bulboff.gif";
-		} else {
-			image.src = "images/pic_bulbon.gif";
-		}
-	}
+
+<script>
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "images/pic_bulboff.gif";
+    } else {
+        image.src = "images/pic_bulbon.gif";
+    }
+}
 </script>
 
 
