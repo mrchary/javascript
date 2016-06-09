b654c56317b687999af4086a16266ba75a4371ee
unknown <Somrat@Somrat.(none)>
2016-06-09 22:33:47 +0600

    change image by js

--- date.php
+++ date.php
@@ -3,5 +3,5 @@
 <button type="button" class="btn btn-primary" onclick="document.getElementById('demo').innerHTML = Date()">
 	Click
 </button>
-<h3 id="demo"></h3>
+<h3 id="demo">Display date & time</h3>
 <?php include("footer.php"); ?>
